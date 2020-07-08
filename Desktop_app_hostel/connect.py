2cd1d755d8bdf2ae8c2aad6a36d5d69dff9a52fc
Sanyam Jain <36095765+sanyam188@users.noreply.github.com>
2020-07-08 10:31:30 +0530

    Update connect.py

--- Desktop_app_hostel/connect.py
+++ Desktop_app_hostel/connect.py
@@ -1,2 +1,2 @@
 from pymongo import *
-client = pymongo.MongoClient("mongodb+srv://Sanyam:<$18sasaA>@cluster0-5nyru.mongodb.net/test?retryWrites=true")
+client = pymongo.MongoClient("mongodb+srv://Sanyam:<$******A>@cluster0-5nyru.mongodb.net/test?retryWrites=true")
